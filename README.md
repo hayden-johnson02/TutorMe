38a52442d47cf8c4fde601653cbbb68436c3f65a
Franklin Glance <franklin.glance@gmail.com>
2023-02-04 14:02:23 -0500

    Removed original Django secret key and added key to local .env

--- README.md
+++ README.md
@@ -10,3 +10,12 @@
 
 
 **Project Choice:** Tutor Me
+
+
+**Heroku address:** https://project-a-03-tutorme.herokuapp.com/
+
+Git: https://git.heroku.com/project-a-03-tutorme.git
+
+
+
+To test heroku locally run `heroku local --port 8000` in the heroku cli
--- tutorme/settings.py
+++ tutorme/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j4gkz))$psa$f99-$30(%%k=zy_@cxqvd%(mwt^#5%bw&m3np6'
+# SECRET_KEY = 'django-insecure-j4gkz))$psa$f99-$30(%%k=zy_@cxqvd%(mwt^#5%bw&m3np6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
