69ab1e18f43adf3f7ff6879efe3f0a18e9b13437
haydenjohnson <cxy6nx@virginia.edu>
2023-03-14 14:46:59 -0400

    made all fields unique in the test profiles

--- tutorme/tests.py
+++ tutorme/tests.py
@@ -7,16 +7,16 @@ from django.contrib.auth import get_user_model
 
 def create_student():
     User = get_user_model()
-    testUser = User.objects.create_user(username="TestStudent", email="mst4k@virginia.edu", password="password123")
-    testStudentProfile = Profile.objects.create(user=testUser, email="mst4k@virginia.edu", first_name="John",
-                                                last_name="Doe", is_student=True, is_tutor=False,
+    testStudentUser = User.objects.create_user(username="TestStudent", email="mst4k@virginia.edu", password="password456")
+    testStudentProfile = Profile.objects.create(user=testStudentUser, email=testStudentUser.email, first_name="John",
+                                                last_name="Dough", is_student=True, is_tutor=False,
                                                 bio="Test Student")
     return testStudentProfile
 
 def create_tutor():
     User = get_user_model()
-    testUser = User.objects.create_user(username= "TestTutor", email="mst3k@virginia.edu", password="password123")
-    testTutorProfile = Profile.objects.create(user=testUser, email="mst3k@virginia.edu", first_name="Jane",
+    testTutorUser = User.objects.create_user(username= "TestTutor", email="mst3k@virginia.edu", password="password123")
+    testTutorProfile = Profile.objects.create(user=testTutorUser, email=testTutorUser.email, first_name="Jane",
                                               last_name="Doe", is_student=False, is_tutor=True,
                                               bio="Test Tutor")
     return testTutorProfile
