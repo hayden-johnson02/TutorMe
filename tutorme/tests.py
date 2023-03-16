62f82fcd491111558329619e22d718d70f9ef701
Tazering <tyler.ys.kim@gmail.com>
2023-03-16 00:37:58 -0400

    added tests to check if MOST of the urls are resolved

--- tutorme/tests.py
+++ tutorme/tests.py
@@ -1,10 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 from tutorme.models import Profile, Course
+from django.urls import reverse, resolve
+from tutorme.views import index, logout_view, login_view, profile_view, edit_profile_view, create_account_view, delete_profile_view, tutor_list, tutor_page, account_type_choice
 from django.contrib.auth import get_user_model
 
 
 # Model Tests
 # Used https://testdriven.io/blog/django-custom-user-model/
+
 
 def create_student():
     User = get_user_model()
@@ -36,9 +39,46 @@ class ProfileModelTests(TestCase):
         self.assertIs(testTutor.is_tutor, True)
 
 # class CourseModelTests(TestCase):
-
+        
 
 # View Tests
 
 
 # Template Tests
+
+# URL Tests
+# Used: https://www.youtube.com/watch?v=0MrgsYswT1c
+
+class TestUrls(SimpleTestCase):
+
+    def test_all_url_is_resolved(self):
+
+        PASS_MESSAGE = " url matches with correct view..."
+        FAIL_MESSAGE = " url does NOT match with correct view..."
+
+        urlDictionary = {
+            "index" : index,
+            "login" : login_view,
+            "logout" : logout_view,
+            "profile" : profile_view,
+            "edit_profile" : edit_profile_view,
+            "delete" : delete_profile_view,
+            "createAccount" : create_account_view,
+            "account_type_choice" : account_type_choice,
+            "view_tutors" : tutor_list,
+
+        }
+
+        for urlName in urlDictionary:
+
+            url = reverse(urlName)
+            try:
+                self.assertEquals(resolve(url).func, urlDictionary[urlName])
+                
+                print("\n" + urlName + PASS_MESSAGE)
+
+            except: 
+                print("\n" + urlName + FAIL_MESSAGE)
+    
+    
+
