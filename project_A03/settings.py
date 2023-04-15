f7183a1c6644bd5cc1c3d88a2c19de65c13c1564
annaegoco <annaegoco11@gmail.com>
2023-04-15 18:57:20 -0400

    tried new way to store static files

--- project_A03/settings.py
+++ project_A03/settings.py
@@ -141,12 +141,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Enable WhiteNoise's GZip compression of static assets.
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # MEDIA_URL = "/media/"
