de224276d0411cb5a190cb84f71ac847cafdb763
annaegoco <annaegoco11@gmail.com>
2023-04-15 19:00:39 -0400

    changed static root

--- project_A03/settings.py
+++ project_A03/settings.py
@@ -137,8 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = BASE_DIR/"staticfiles"
+STATIC_ROOT = BASE_DIR/"static"
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Enable WhiteNoise's GZip compression of static assets.
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
